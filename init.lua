c67d6ea4f7abe7163d2d3a015b03c472b11c30d6
Victor Nasonov <qxuken@gmail.com>
2024-10-08 20:05:07 +0300

    feat: Tabline config and workspace keybinds

--- init.lua
+++ init.lua
@@ -1,64 +1,128 @@
-local cyberdream_colors = require("./coloschemes/cyberdream")
 local wezterm = require("wezterm")
 local act = wezterm.action
 
+local cyberdream_colors = require("./coloschemes/cyberdream")
+
 local config = wezterm.config_builder()
 
-config.colors = cyberdream_colors
-
-config.font = wezterm.font("SauceCodePro Nerd Font Mono")
-config.font_size = 20.0
-
-config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 1000 }
-config.keys = {
-	{ key = "(", mods = "LEADER", action = act.SwitchWorkspaceRelative(-1) },
-	{ key = ")", mods = "LEADER", action = act.SwitchWorkspaceRelative(1) },
-	{ key = "l", mods = "LEADER", action = act.ShowLauncher },
-	{ key = "c", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
-	{ key = "n", mods = "LEADER", action = act.ActivateTabRelative(1) },
-	{ key = "p", mods = "LEADER", action = act.ActivateTabRelative(-1) },
-}
-
-if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	config.default_prog = { "pwsh", "-l", "-NoLogo" }
-end
-
-wezterm.plugin.require("https://github.com/yriveiro/wezterm-tabs").apply_to_config(config, {
-	ui = {
-		separators = {
-			arrow_solid_left = "|",
-			arrow_solid_right = "|",
-			arrow_thin_left = "|",
-			arrow_thin_right = "|",
+local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
+bar.apply_to_config(config, {
+	padding = {
+		top = 0,
+	},
+	separator = {
+		space = 1,
+		left_icon = "",
+		right_icon = "",
+	},
+	modules = {
+		workspace = {
+			color = 5,
+		},
+		pane = {
+			enabled = false,
+		},
+		username = {
+			enabled = false,
+		},
+		hostname = {
+			enabled = false,
+		},
+		cwd = {
+			enabled = false,
 		},
 	},
 })
 
+config.window_padding = {
+	left = 1,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	-- config.window_background_opacity = 0.85
+	-- config.win32_system_backdrop = "Acrylic"
+	config.window_background_opacity = 0.0
+	config.win32_system_backdrop = "Mica"
+	-- config.win32_system_backdrop = "Tabbed"
+else
+	config.window_background_opacity = 0.95
+	config.macos_window_background_blur = 20
+end
+
+config.font = wezterm.font("SauceCodePro Nerd Font Mono")
+config.font_size = 19.0
+config.command_palette_font_size = 22.0
+config.command_palette_bg_color = cyberdream_colors.background
+config.command_palette_fg_color = cyberdream_colors.foreground
+
+config.color_schemes = {
+	Cyberdream = cyberdream_colors,
+}
+config.color_scheme = "Cyberdream"
+config.colors = cyberdream_colors
+
 config.colors.tab_bar = {
-	background = cyberdream_colors.background,
+	background = "transparent",
 	active_tab = {
-		bg_color = cyberdream_colors.background,
+		bg_color = "transparent",
 		fg_color = cyberdream_colors.brights[6],
 		intensity = "Bold",
 	},
 	inactive_tab = {
-		bg_color = cyberdream_colors.background,
+		bg_color = "transparent",
 		fg_color = cyberdream_colors.selection_bg,
 	},
 	inactive_tab_hover = {
-		bg_color = cyberdream_colors.indexed[16],
+		bg_color = "transparent",
 		fg_color = cyberdream_colors.selection_bg,
 		italic = true,
 	},
 	new_tab = {
-		bg_color = cyberdream_colors.background,
+		bg_color = "transparent",
 		fg_color = cyberdream_colors.selection_bg,
 	},
 	new_tab_hover = {
 		bg_color = cyberdream_colors.cursor_bg,
 		fg_color = cyberdream_colors.cursor_fg,
-		italic = false,
 	},
 }
+
+config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+	{ key = "F11", action = wezterm.action.ToggleFullScreen },
+	{ key = "(", mods = "LEADER|SHIFT", action = act.SwitchWorkspaceRelative(-1) },
+	{ key = ")", mods = "LEADER|SHIFT", action = act.SwitchWorkspaceRelative(1) },
+	{ key = "l", mods = "LEADER", action = act.ShowLauncher },
+	{ key = "c", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "w", mods = "LEADER", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
+	{
+		key = "n",
+		mods = "LEADER",
+		action = act.PromptInputLine({
+			description = wezterm.format({
+				{ Attribute = { Intensity = "Bold" } },
+				{ Foreground = { AnsiColor = "Fuchsia" } },
+				{ Text = "Enter name for new workspace" },
+			}),
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					window:perform_action(
+						act.SwitchToWorkspace({
+							name = line,
+						}),
+						pane
+					)
+				end
+			end),
+		}),
+	},
+}
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	config.default_prog = { "pwsh", "-l", "-NoLogo" }
+end
 
 return config
