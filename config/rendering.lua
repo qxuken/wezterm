cc1cf7cb4d1608ce66371cdca98e5f5ab305e436
Nasonov V <qxuken@qxc-mbp-m3.local>
2024-11-29 17:30:21 +0300

    tweak(macos): enable high performance power

--- config/rendering.lua
+++ config/rendering.lua
@@ -1,5 +1,3 @@
-local wezterm = require("wezterm")
-
 local M = {}
 
 -- conforming to https://github.com/wez/wezterm/commit/e4ae8a844d8feaa43e1de34c5cc8b4f07ce525dd
@@ -7,10 +5,7 @@ M.apply_to_config = function(c)
 	c.front_end = "WebGpu"
 	c.max_fps = 144
 	c.animation_fps = 144
-
-	if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-		c.webgpu_power_preference = "HighPerformance"
-	end
+	c.webgpu_power_preference = "HighPerformance"
 end
 
 return M
