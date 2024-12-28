190b023f538f78ee77a1af661a2ab4b928ab7bda
Nasonov Viktor <qxuken@gmail.com>
2024-12-28 17:18:57 +0300

    tweak: remove front_end forcing

--- config/rendering.lua
+++ config/rendering.lua
@@ -2,7 +2,7 @@ local M = {}
 
 -- conforming to https://github.com/wez/wezterm/commit/e4ae8a844d8feaa43e1de34c5cc8b4f07ce525dd
 M.apply_to_config = function(c)
-	c.front_end = "WebGpu"
+	-- c.front_end = "WebGpu"
 	c.max_fps = 144
 	c.animation_fps = 144
 	c.webgpu_power_preference = "HighPerformance"
